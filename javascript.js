758c3cf883bf16c987f5b7ab317222ccc809a7b9
JP Castrillon <jpcas001@gmail.com>
2021-01-20 14:28:49 -0500

    adding images to the front page adjusting CSS

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
-let t1 = new TimelineMax({opUpdate:updatePercentage});
-const controller = new ScrollMagic.Controller();
+// let t1 = new TimelineMax({opUpdate:updatePercentage});
+// const controller = new ScrollMagic.Controller();
 
-t1.from("blockquote", .5, {x:200, opacity: 0})
-TouchList.from('#office', 1, {x:-200})
+// t1.from("blockquote", .5, {x:200, opacity: 0})
+// TouchList.from('#office', 1, {x:-200})
 
-const scene = new ScrollMagic.Scene({
-    triggerElement: ".sticky", 
-    triggerHook: "onLeave",
-    duration: "100%"
-})
+// const scene = new ScrollMagic.Scene({
+//     triggerElement: ".sticky", 
+//     triggerHook: "onLeave",
+//     duration: "100%"
+// })
 
-.setPin(".sticky")
-.setTween(t1)
-    .addTo(controller);
+// .setPin(".sticky")
+// .setTween(t1)
+//     .addTo(controller);
 
-function updatePercentage() {
-    t1.progress();
-    console.log(t1.progress)
-}
+// function updatePercentage() {
+//     t1.progress();
+//     console.log(t1.progress)
+// }
