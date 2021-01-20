6ec4345424a08908c01076c53551c44111cfcc4f
JP Castrillon <jpcas001@gmail.com>
2021-01-20 16:57:49 -0500

    debugged js file

--- javascript.js
+++ javascript.js
@@ -1,37 +1,14 @@
-// let t1 = new TimelineMax({opUpdate:updatePercentage});
-// const controller = new ScrollMagic.Controller();
-
-// t1.from("blockquote", .5, {x:200, opacity: 0})
-// TouchList.from('#office', 1, {x:-200})
-
-// const scene = new ScrollMagic.Scene({
-//     triggerElement: ".sticky", 
-//     triggerHook: "onLeave",
-//     duration: "100%"
-// })
-
-// .setPin(".sticky")
-// .setTween(t1)
-//     .addTo(controller);
-
-// function updatePercentage() {
-//     t1.progress();
-//     console.log(t1.progress)
-// }
-
 const flightPath = { 
     curviness: 0, 
     autoRotate: true, 
-    values: [
-        {x: 100, y: -20}
-    ]
-}
+    values: [{ x: 100, y: -20 }, { x:300, y: 10}]
+};
 
 const tween = new TimelineLite();
 
 tween.add(
-    Tweenlite.to('.jpbikes', 1, {
-        bazier: flightPath, 
+    TweenLite.to(".jp-bike", 1, {
+        bezier: flightPath, 
         ease: Power1.easeInOut
     })
 );
